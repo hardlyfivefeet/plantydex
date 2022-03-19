e58f93edae0a552c7c0297851f65d2ab41bc66eb
Evan Mitchell <evan.m.mitchell@gmail.com>
2022-03-18 18:38:54 -0700

    Add working API support

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-// Mock JSON data that would be received from API
-const mockJson = `
-{
-  "id": 3010636,
-  "plant": {
-    "name": "Buddleja davidii",
-    "url": "https://en.wikipedia.org/wiki/Buddleja_davidii",
-    "common_name": "Butterfly-Bush"
-  },
-  "probability": 0.9277143686644568,
-  "confidence": 0.9798141922207031,
-  "similar_images": [
-    {
-      "id": "605775c7cb05e8463f7f8463b0fd915c",
-      "similarity": 0.9318117206281141,
-      "url": "https://storage.googleapis.com/plant_id_images/similar_images/2019_05/images/Buddleja davidii/605775c7cb05e8463f7f8463b0fd915c.jpg",
-      "url_small": "https://storage.googleapis.com/plant_id_images/similar_images/2019_05/images/Buddleja davidii/605775c7cb05e8463f7f8463b0fd915c.small.jpg"
-    },
-    {
-      "id": "890cf5e5b94a255ea4e517d785f53481",
-      "similarity": 0.9316171609752804,
-      "url": "https://storage.googleapis.com/plant_id_images/similar_images/2019_05/images/Buddleja davidii/890cf5e5b94a255ea4e517d785f53481.jpg",
-      "url_small": "https://storage.googleapis.com/plant_id_images/similar_images/2019_05/images/Buddleja davidii/890cf5e5b94a255ea4e517d785f53481.small.jpg"
-    }
-  ],
-  "confirmed": false
-}
-`;
-
-
 class Plant {
   constructor(image, info) {
     this.image = image;
@@ -51,19 +21,26 @@ for (let plant of plants) {
   addToPlantydex(plant);
 }
 
-fileInput.addEventListener("change", function (event) {
+fileInput.addEventListener("change", event => {
   let plantImage = event.target.files[0];
   let reader = new FileReader();
-  reader.onload = function () {
-    let apiResponseInfo = JSON.parse(mockJson);
-    let plant = new Plant(reader.result, apiResponseInfo["plant"]);
-    if (!plants.find(o => o.info["name"] === plant.info["name"])) {
-      plants.push(plant);
-      localStorage.setItem("plants", JSON.stringify(plants));
-      addToPlantydex(plant);
-    } else {
-      alert("Plant already collected in Plantydex!");
-    }
+  reader.onload = event => {
+    let image = event.target.result;
+    identify(image).then(data => {
+      if (data["is_plant"]) {
+        let plant = new Plant(image, data["suggestions"][0]["plant_details"]);
+        if (!plants.find(o => o.info["scientific_name"] === plant.info["scientific_name"])) {
+          plants.push(plant);
+          localStorage.setItem("plants", JSON.stringify(plants));
+          addToPlantydex(plant);
+          showDetails(plant);
+        } else {
+          alert("This plant is already in your Plantydex!");
+        }
+      } else {
+        alert("This is not a plant!");
+      }
+    });
   };
   reader.readAsDataURL(plantImage);
 });
@@ -72,13 +49,29 @@ function addToPlantydex(plant) {
   let img = new Image();
   img.src = plant.image;
   img.className = "plantydexItem";
-  img.addEventListener("click", function () {
-    thumbnailImage.src = plant.image;
-    commonName.textContent = plant.info["common_name"];
-    scientificName.textContent = plant.info["name"];
-    readMore.href = plant.info["url"];
-    readMore.textContent = "Read More";
-  });
+  img.addEventListener("click", () => showDetails(plant));
   plantydexItems.appendChild(img);
   score.textContent = plants.length;
+}
+
+function showDetails(plant) {
+  thumbnailImage.src = plant.image;
+  commonName.textContent = plant.info["common_names"][0];
+  scientificName.textContent = plant.info["scientific_name"];
+  readMore.href = plant.info["url"];
+  readMore.textContent = "Read More";
+}
+
+async function identify(image) {
+  let data = {
+    api_key: "PF5tQJVC4uHLZCNNGEi02KdNldLfYBb5k50mKgb1lzEaZ77sUg",
+    images: [image],
+    plant_details: ["common_names", "url", "scientific_name"]
+  };
+  let args = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data)
+  };
+  return fetch("https://api.plant.id/v2/identify", args).then(response => response.json());
 }
